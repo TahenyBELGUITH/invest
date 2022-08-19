18cc8d5307d1d5d13cb9cbaa0642fdc7f0dfb686
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-08-19 16:24:10 +0100

    Add static layouts

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked!!!");
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
